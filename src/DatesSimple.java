0a1f9456553cfbc6658ff6ea468fde79ed2be38e
Ningrui Zhang <ningrui1227@gmail.com>
2024-11-08 08:28:48 +0000

    add comment lines to DatesSimple.java

--- src/DatesSimple.java
+++ src/DatesSimple.java
@@ -11,9 +11,11 @@ public class DatesSimple {
         int period = 0;
         int resetFromy = fromy - 2000; // adjust the fromy to be in range [0, 127]
         int resetToy = toy - 2000; // adjust the toy to be in range [0, 127]
-
+//      pack the "from" date (fromy/m/d) into a 16 bits integer
         int packedDate = (resetFromy & 0x7f) << 9 | (fromm & 0x0f) << 5 | (fromd & 0x1f);
+//      shift the packedDate to the left 16 bits to be at higher bits
         packedDate = packedDate << 16;
+//      pack the "to" date (toy/m/d) into the lower 16 bits of packedDate.
         packedDate = packedDate | (resetToy & 0x7f) << 9 | (tom & 0x0f) << 5 | (tod & 0x1f);
         period = packedDate;
         fromy = fromm = fromd = toy = tom = tod = 0;
